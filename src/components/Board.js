4ecadec2a547b6ba5f680c2a1def521f6d3a0ec7
Subhasree Vadukoot <51084285+subhasreevadukoot@users.noreply.github.com>
2021-04-24 21:26:26 +0100

    Added board for the game

---
+++ src/components/Board.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Square from './Square'
+
+const Board = () => {
+    return (
+        <div>
+            <div>
+                
+                <Square value={0}/>
+                <Square value={0}/>
+                <Square value={0}/>
+            </div>
+            <div>
+                <Square value={0}/>
+                <Square value={0}/>
+                <Square value={0}/>
+            </div>
+            <div>
+                <Square value={0}/>
+                <Square value={0}/>
+                <Square value={0}/>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Board
